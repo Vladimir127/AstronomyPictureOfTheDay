28b69181d898bd39ffba1659b337746b14e3ffac
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2022-01-21 15:14:50 +0300

    Реализована активность-слайдер

---
+++ app/src/main/java/com/example/apod/Slide1Fragment.kt
@@ -0,0 +1,16 @@
+package com.example.apod
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Slide1Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_slide1, container, false)
+    }
+}
